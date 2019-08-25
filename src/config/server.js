9638980f44c3497efe1f8519b16044d713143403
Raphael <raphaeldeoliveiramoura@gmail.com>
2019-08-24 23:00:36 -0300

    env variable name changed

--- src/config/server.js
+++ src/config/server.js
@@ -6,7 +6,7 @@ const serverConfigurations = {
   database: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
+    database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     dialect: process.env.DATABASE_DIALECT
   }
--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,6 +1,25 @@
 const Sequelize = require('sequelize');
 const databaseConfigurations = require('../config/database');
 
-const sequelize = new Sequelize(databaseConfigurations);
+const {
+  database,
+  username,
+  password,
+  host,
+  dialect,
+  port
+} = databaseConfigurations;
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
+  port,
+  define: {
+    paranoid: false,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true
+  }
+});
 
 module.exports = sequelize;
