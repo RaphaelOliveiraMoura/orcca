722b373b3e1aedd08b291da342a31cae0a2d5870
Raphael <raphaeldeoliveiramoura@gmail.com>
2019-08-26 21:20:53 -0300

    start of header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <ul>
+        <li>Home</li>
+        <li>Agenda</li>
+        <li>Relatórios</li>
+        <li>Contato</li>
+      </ul>
+    </div>
+  );
+}
