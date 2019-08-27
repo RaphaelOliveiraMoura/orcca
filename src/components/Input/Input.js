3eafb8c05d0b100548a135e9ae52d523bd394d25
Raphael <raphaeldeoliveiramoura@gmail.com>
2019-08-26 21:20:45 -0300

    useeffect to handle error input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import InputMask from 'react-input-mask';
 import types from './validators';
 
 import './styles.css';
 
 export default function Input(props) {
-  const [inputText, setInputText] = useState('');
+  const [invalidMessage, setInvalidMessage] = useState(false);
+
+  useEffect(() => {
+    setInvalidMessage(!!props.error);
+  }, [props.error]);
 
   function handleInputChange(event) {
-    const text = event.target.value;
     const $input = event.target;
-    setInputText(text);
-
-    if ($input.validity.valid) $input.classList.remove('invalid');
+    const text = $input.value;
+    props.setValue(text);
+    if ($input.validity.valid) setInvalidMessage(false);
   }
 
   function handleInvalidInput(event) {
     event.preventDefault();
-
-    const $input = event.target;
-    $input.classList.add('invalid');
+    setInvalidMessage(true);
   }
 
   return (
@@ -31,15 +32,19 @@ export default function Input(props) {
         className="input"
         type={'text'}
         onChange={handleInputChange}
-        value={inputText}
+        value={props.value}
         spellCheck="false"
         onInvalid={handleInvalidInput}
         required
       />
-      <span className={inputText ? 'placeholder active' : 'placeholder'}>
+      <span className={props.value ? 'placeholder active' : 'placeholder'}>
         {props.placeholder}
       </span>
-      <span className="invalid">Campo Inválido</span>
+      {invalidMessage && (
+        <span className="invalid">
+          {props.errorMessage || 'Campo Inválido'}
+        </span>
+      )}
     </div>
   );
 }
