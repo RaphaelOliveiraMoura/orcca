fcec202d1a10f08e6674008fcedaf40fea28fab5
Raphael <raphaeldeoliveiramoura@gmail.com>
2019-08-29 22:51:11 -0300

    resolve jwt token conflict

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,23 +2,6 @@ const jwt = require('jsonwebtoken');
 const { protectKey } = require('../config/server');
 const expirationTime = '1h';
 
-<<<<<<< HEAD
-function generateToken(userId) {
-  return jwt.sign(
-    {
-      userId
-    },
-    protectKey,
-    { expiresIn: expirationTime }
-  );
-}
-
-async function verifyToken(token) {
-  try {
-    const { userId } = jwt.verify(token, protectKey);
-    if (!userId) return false;
-    return userId;
-=======
 function generateToken(id, rule) {
   return jwt.sign({ id, rule }, protectKey, { expiresIn: expirationTime });
 }
@@ -28,7 +11,6 @@ function verifyToken(token) {
     const { id, rule } = jwt.verify(token, protectKey);
     if (!id || !rule) return false;
     return { id, rule };
->>>>>>> raphael/migrations
   } catch (error) {
     return false;
   }
