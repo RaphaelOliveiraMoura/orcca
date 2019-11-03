dd91d8bc04ab14c515b25907c02fc9dfbc449a4d
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-11-03 13:46:16 -0300

    refact: @backend/Add dockerfile with docker-compose
    Add a container to run the api and another to run postgres database.
    And a network for comunication between them.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: process.env.DB_DIALECT || 'postgres',
+  dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
