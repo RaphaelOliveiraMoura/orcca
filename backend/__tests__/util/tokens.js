912d8e5d805df5932abd40140b734994d0f513e9
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2020-01-22 22:48:41 -0300

    styles: Remove log to gettoken function

--- backend/__tests__/util/tokens.js
+++ backend/__tests__/util/tokens.js
@@ -5,23 +5,19 @@ import factory from '../factories';
 import app from '~/app';
 
 export default async function getToken({ rule = 'admin' }) {
-  try {
-    const rules = {
-      admin: 1,
-      socialWorker: 2,
-      clerk: 3,
-    };
+  const rules = {
+    admin: 1,
+    socialWorker: 2,
+    clerk: 3,
+  };
 
-    const user = await factory.create('User', {
-      rule_id: rules[rule],
-    });
+  const user = await factory.create('User', {
+    rule_id: rules[rule],
+  });
 
-    const response = await request(app)
-      .post('/sessions')
-      .send({ login: user.login, password: user.password });
+  const response = await request(app)
+    .post('/sessions')
+    .send({ login: user.login, password: user.password });
 
-    return response.body.token;
-  } catch (error) {
-    console.log(error);
-  }
+  return response.body.token;
 }
